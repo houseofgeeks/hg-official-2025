c693b0bf5b0a2c5591dce9c2d34b306b7ae4def0
Atharvjainn <jainatharv20@gmail.com>
2025-12-28 17:12:31 +0530

    Fixed Logout issue

--- app/api/hgadmin/logout/route.ts
+++ app/api/hgadmin/logout/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
-export async function POST() {
+export async function POST() { 
   const res = NextResponse.json({ success: true });
-  res.cookies.set('hgadmin_auth', '', { path: '/hgadmin', expires: new Date(0) });
+  res.cookies.set('hgadmin_auth', '', { path: '/', expires: new Date(0) });
   return res;
 }
